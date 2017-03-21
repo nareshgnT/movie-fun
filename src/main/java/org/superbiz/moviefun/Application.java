da9a3d1400f92a29758b4f41baf40d7838e7634e
nareshgn <nareshgn@gmail.com>
2017-03-21 14:36:38 -0600

    first push

---
+++ src/main/java/org/superbiz/moviefun/Application.java
@@ -0,0 +1,18 @@
+package org.superbiz.moviefun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+    @Bean
+    public ServletRegistrationBean servletRegistrationBean(ActionServlet actionServlet){
+        return new ServletRegistrationBean(actionServlet, "/moviefun/*");
+    }
+}
